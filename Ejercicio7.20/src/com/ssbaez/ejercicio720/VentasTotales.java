ae82bc5d5b1f69c5f53541f8db80040c7c0db30f
Saúl Báez <ss.baez@comunidad.unam.mx>
2018-04-02 18:56:18 -0500

    Initial commit

---
+++ Ejercicio7.20/src/com/ssbaez/ejercicio720/VentasTotales.java
@@ -0,0 +1,86 @@
+package com.ssbaez.ejercicio720;
+
+public class VentasTotales {
+	
+	public static void main(String[] args) {
+		
+		//Filas: productos
+		//Columnas: vendedores
+		int totalV1 = 0;
+		int totalV2 = 0;
+		int totalV3 = 0;
+		int totalV4 = 0;
+		
+		int[][] ventas = {{300, 200, 570, 480}, 
+						  {850, 100, 405, 350}, 
+						  {950, 220, 523, 454}, 
+						  {230, 800, 546, 654},
+						  {343, 325, 325, 742}};
+		
+		System.out.println("VENTAS TOTALES:");
+		System.out.print("            ");
+		
+		//encabezado
+		for(int i = 0; i < ventas[0].length; i++) {
+			System.out.printf("Vendedor %d   ", i + 1);
+		}
+		
+		System.out.println("TOTAL");
+		
+		for(int producto = 0; producto < ventas.length; producto++) {
+			
+			System.out.printf("Producto %d", producto + 1);
+			
+			for(int vendedor : ventas[producto])
+				System.out.printf("%11d", vendedor);
+			
+			int total = obtTotal(ventas[producto]);
+			System.out.printf("%,16d%n", total);
+		}
+		
+		System.out.print("TOTAL        ");
+		
+		int contador = 1;
+		int col = 0;
+		int totalTotal = 0;
+		
+		while(contador < 5) {
+			
+			int fcol = col;
+			int totalV = 0;
+			
+			for(int k = 0; k < ventas.length; k++) {
+				
+				for(int l = col; l == fcol; l++) {
+					
+					totalV += ventas[k][l];
+					
+				}//Fin de for anidado
+				
+			}//Fin de for principal
+			
+			totalTotal += totalV;
+			System.out.printf("%,10d ", totalV);
+			
+			col++;
+			contador++;
+			
+		}//Fin de while
+		
+		System.out.printf("%,13d ", totalTotal);
+		
+	}//Fin de metodo main
+	
+	public static int obtTotal(int[] matriz) {
+		
+		int total = 0;
+		
+		for(int i : matriz) {
+			total += i;
+		}
+		
+		return total;
+		
+	}//Fin de metodo obtTotal
+	
+}//Fin de declaracion de clase
